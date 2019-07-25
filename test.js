09b7716cf22918ca2d6cbdc1c62b1a100ff8d02c
Elias <p.elias.graf@gmail.com>
2019-07-25 15:50:59 +0200

    removed test file

--- test.js
+++
@@ -1,8 +0,0 @@
-const { msg } = require('./build/index.js');
-
-msg('Foreground is red!').fg('red').log();
-msg('Background is red!').bg('red').log();
-
-
-msg('White bg, black fg').bg('white').fg('black').log();
-msg('This').fg('yellow').tail(' is').fg('red').tail(' colorful!').fg('blue').log();
